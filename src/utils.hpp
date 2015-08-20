1f5d1995ec4fd73242d488e8647a98c3a6d52b37
hatsusato <knightspm5@gmail.com>
2015-08-20 16:54:20 +0900

    Add less_element and greater_element

--- src/utils.hpp
+++ src/utils.hpp
@@ -36,5 +36,13 @@ template <size_t N>
 bool same_element(const Coord& coord, int value) {
   return std::get<N>(coord) == value;
 }
+template <size_t N>
+bool less_element(const Coord& lhs, const Coord& rhs) {
+  return std::get<N>(lhs) < std::get<N>(rhs);
+}
+template <size_t N>
+bool greater_element(const Coord& lhs, const Coord& rhs) {
+  return std::get<N>(lhs) > std::get<N>(rhs);
+}
 
 #endif  // PIET_I_UTILS_HPP

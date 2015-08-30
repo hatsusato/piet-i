653041211a0b3dd7b3dedc37d4f9f59b08c06869
hatsusato <knightspm5@gmail.com>
2015-08-31 01:51:55 +0900

    Add stack class

--- src/interpreter.hpp
+++ src/interpreter.hpp
@@ -1,4 +1,10 @@
 #ifndef PIET_I_INTERPRETER_HPP
 #define PIET_I_INTERPRETER_HPP
 
+#include <stack>
+#include <vector>
+
+class Stack : public std::stack<int, std::vector<int> > {
+};
+
 #endif  // PIET_I_INTERPRETER_HPP

a8d35221a4343d6f0b2c858654bf2d28f24ca892
hatsusato <knightspm5@gmail.com>
2015-08-31 01:59:21 +0900

    Add push and pop

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1,1 +1,10 @@
 #include "interpreter.hpp"
+
+void Stack::push_command(size_t number) {
+  push(static_cast<int>(number));
+}
+void Stack::pop_command() {
+  if (!empty()) {
+    pop();
+  }
+}
--- src/interpreter.hpp
+++ src/interpreter.hpp
@@ -5,6 +5,9 @@
 #include <vector>
 
 class Stack : public std::stack<int, std::vector<int> > {
+ public:
+  void push_command(size_t number);
+  void pop_command();
 };
 
 #endif  // PIET_I_INTERPRETER_HPP

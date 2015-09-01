54ffcea1e5e96f3b3c411490da0eaba813a57eaf
hatsusato <knightspm5@gmail.com>
2015-09-02 03:18:33 +0900

    Add support utility

--- src/coord.cpp
+++ src/coord.cpp
@@ -1,5 +1,56 @@
 #include "coord.hpp"
+#include <cassert>
 #include <algorithm>
+#include <functional>
+
+namespace /* unnamed */ {
+std::tuple<std::function<std::vector<int>(const Coordinates&)>,
+           std::function<Coord(int)>,
+           std::function<bool(const int&, const int&)> >
+generate_utilities(Direction direction, int value) {
+  const auto filter_horz =
+      [value](const Coordinates& coords) -> std::vector<int> {
+    std::vector<int> result;
+    for (auto&& coord : coords) {
+      if (coord.x() == value) {
+        result.push_back(coord.y());
+      }
+    }
+    return result;
+  };
+  const auto filter_vert =
+      [value](const Coordinates& coords) -> std::vector<int> {
+    std::vector<int> result;
+    for (auto&& coord : coords) {
+      if (coord.y() == value) {
+        result.push_back(coord.x());
+      }
+    }
+    return result;
+  };
+  const auto make_horz = [value](int minmax) -> Coord {
+    return Coord(value, minmax);
+  };
+  const auto make_vert = [value](int minmax) -> Coord {
+    return Coord(minmax, value);
+  };
+  const auto less = std::less<int>();
+  const auto greater = std::greater<int>();
+  switch (direction) {
+    case DP::RIGHT:
+      return std::make_tuple(filter_horz, make_horz, less);
+    case DP::DOWN:
+      return std::make_tuple(filter_vert, make_vert, greater);
+    case DP::LEFT:
+      return std::make_tuple(filter_horz, make_horz, greater);
+    case DP::UP:
+      return std::make_tuple(filter_vert, make_vert, less);
+    default:
+      assert(false);
+      return std::make_tuple(nullptr, nullptr, nullptr);
+  }
+}
+}  // namespace /* unnamed */
 
 Coord::Coord()
     : Base(0, 0)

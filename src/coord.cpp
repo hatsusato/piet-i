21f86216e988dd0010e708cb3056194d99be143b
hatsusato <knightspm5@gmail.com>
2015-09-02 01:49:36 +0900

    Add accessors

--- src/coord.cpp
+++ src/coord.cpp
@@ -6,3 +6,15 @@ Coord::Coord()
 Coord::Coord(int x, int y)
     : Base(x, y)
 {}
+int& Coord::x() {
+  return std::get<0>(*this);
+}
+const int& Coord::x() const {
+  return std::get<0>(*this);
+}
+int& Coord::y() {
+  return std::get<1>(*this);
+}
+const int& Coord::y() const {
+  return std::get<1>(*this);
+}
--- src/coord.hpp
+++ src/coord.hpp
@@ -8,6 +8,10 @@ class Coord : public std::tuple<int, int> {
  public:
   Coord();
   Coord(int x, int y);
+  int& x();
+  const int& x() const;
+  int& y();
+  const int& y() const;
 };
 
 #endif  // PIET_I_COORD_HPP

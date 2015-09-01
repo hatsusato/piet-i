d326aaead21896ea9890c692337872655efb3890
hatsusato <knightspm5@gmail.com>
2015-09-02 01:09:31 +0900

    Add Coord class derived from tuple<int, int>

--- src/coord.cpp
+++ src/coord.cpp
@@ -1,1 +1,8 @@
 #include "coord.hpp"
+
+Coord::Coord()
+    : Base(0, 0)
+{}
+Coord::Coord(int x, int y)
+    : Base(x, y)
+{}
--- src/coord.hpp
+++ src/coord.hpp
@@ -1,4 +1,13 @@
 #ifndef PIET_I_COORD_HPP
 #define PIET_I_COORD_HPP
 
+#include <tuple>
+
+class Coord : public std::tuple<int, int> {
+  using Base = std::tuple<int, int>;
+ public:
+  Coord();
+  Coord(int x, int y);
+};
+
 #endif  // PIET_I_COORD_HPP

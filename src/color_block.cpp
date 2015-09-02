3f77ade7b21ff0b58338ef3a92766bba604e0326
hatsusato <knightspm5@gmail.com>
2015-09-02 19:25:11 +0900

    Replace ColorBlockBase with BlockBase

--- src/color_block.cpp
+++ src/color_block.cpp
@@ -3,21 +3,21 @@
 #include <iterator>
 #include <functional>
 
-ColorBlockBase::ColorBlockBase() {}
-ColorBlockBase::~ColorBlockBase() {}
-size_t ColorBlockBase::codel_size() const {
+BlockBase::BlockBase() {}
+BlockBase::~BlockBase() {}
+size_t BlockBase::codel_size() const {
   return 0;
 }
-const ColorBlockBase* ColorBlockBase::address() const {
+const BlockBase* BlockBase::address() const {
   return this;
 }
-bool ColorBlockBase::is_colored() const {
+bool BlockBase::is_colored() const {
   return false;
 }
-bool ColorBlockBase::is_white() const {
+bool BlockBase::is_white() const {
   return false;
 }
-bool ColorBlockBase::is_black() const {
+bool BlockBase::is_black() const {
   return false;
 }
 
@@ -31,13 +31,13 @@ Codel ColorBlock::codel() const {
 size_t ColorBlock::codel_size() const {
   return codel_size_;
 }
-const ColorBlockBase* ColorBlock::next(Direction direction,
-                                       Choose choose) const {
+const BlockBase* ColorBlock::next(Direction direction,
+                                  Choose choose) const {
   const auto d = static_cast<int>(direction);
   const auto c = static_cast<int>(choose);
   return next_[d][c];
 }
-void ColorBlock::set_next(const ColorBlockBase* next,
+void ColorBlock::set_next(const BlockBase* next,
                           Direction direction, Choose choose) {
   const auto d = static_cast<int>(direction);
   const auto c = static_cast<int>(choose);
@@ -52,20 +52,20 @@ BlackBlock::BlackBlock()
 Codel BlackBlock::codel() const {
   return Codel(Color::BLACK, Brightness::NORMAL);
 }
-const ColorBlockBase* BlackBlock::next(Direction, Choose) const {
+const BlockBase* BlackBlock::next(Direction, Choose) const {
   return this;
 }
 bool BlackBlock::is_black() const {
   return true;
 }
 
-WhiteBlock::WhiteBlock(const ColorBlockBase* next)
+WhiteBlock::WhiteBlock(const BlockBase* next)
     : next_(next)
 {}
 Codel WhiteBlock::codel() const {
   return Codel(Color::WHITE, Brightness::NORMAL);
 }
-const ColorBlockBase* WhiteBlock::next(Direction, Choose) const {
+const BlockBase* WhiteBlock::next(Direction, Choose) const {
   return next_;
 }
 bool WhiteBlock::is_white() const {
--- src/color_block.hpp
+++ src/color_block.hpp
@@ -6,58 +6,58 @@
 #include "connected_codel.hpp"
 #include "memory.hpp"
 
-class ColorBlockBase {
+class BlockBase {
  public:
-  virtual ~ColorBlockBase() = 0;
+  virtual ~BlockBase() = 0;
   virtual Codel codel() const = 0;
   virtual size_t codel_size() const;
-  virtual const ColorBlockBase* next(Direction direction,
-                                     Choose choose) const = 0;
-  const ColorBlockBase* address() const;
+  virtual const BlockBase* next(Direction direction,
+                                Choose choose) const = 0;
+  const BlockBase* address() const;
   virtual bool is_colored() const;
   virtual bool is_white() const;
   virtual bool is_black() const;
  protected:
-  ColorBlockBase();
+  BlockBase();
  private:
-  ColorBlockBase(ColorBlockBase&&) = delete;
+  BlockBase(BlockBase&&) = delete;
 };
-using ColorBlockPtr = std::unique_ptr<ColorBlockBase>;
+using ColorBlockPtr = std::unique_ptr<BlockBase>;
 
-class ColorBlock : public ColorBlockBase {
+class ColorBlock : public BlockBase {
  public:
   explicit ColorBlock(const ConnectedCodel& connected);
   Codel codel() const override;
   size_t codel_size() const override;
-  const ColorBlockBase* next(Direction direction,
-                             Choose choose) const override;
-  void set_next(const ColorBlockBase* next,
+  const BlockBase* next(Direction direction,
+                        Choose choose) const override;
+  void set_next(const BlockBase* next,
                 Direction direction, Choose choose);
   bool is_colored() const override;
  private:
   Codel codel_;
   size_t codel_size_;
-  const ColorBlockBase* next_[4][2];
+  const BlockBase* next_[4][2];
 };
 
-class BlackBlock : public ColorBlockBase {
+class BlackBlock : public BlockBase {
  public:
   BlackBlock();
   Codel codel() const override;
-  const ColorBlockBase* next(Direction direction,
-                             Choose choose) const override;
+  const BlockBase* next(Direction direction,
+                        Choose choose) const override;
   bool is_black() const override;
 };
 
-class WhiteBlock : public ColorBlockBase {
+class WhiteBlock : public BlockBase {
  public:
-  explicit WhiteBlock(const ColorBlockBase* next);
+  explicit WhiteBlock(const BlockBase* next);
   Codel codel() const override;
-  const ColorBlockBase* next(Direction direction,
-                             Choose choose) const override;
+  const BlockBase* next(Direction direction,
+                        Choose choose) const override;
   bool is_white() const override;
  private:
-  const ColorBlockBase* next_;
+  const BlockBase* next_;
 };
 
 #endif  // PIET_I_COLOR_BLOCK_HPP

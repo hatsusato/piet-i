f3c65968392e80c5e8fcc3a2791390b93ce56276
hatsusato <knightspm5@gmail.com>
2015-09-03 19:52:05 +0900

    Update show_pixel

--- src/visualize.cpp
+++ src/visualize.cpp
@@ -68,23 +68,20 @@ Index back_index(const Codel& codel) {
 
 void show_pixel(const Codel& codel) {
   static const char* fore[] =
-      {"\x1b[30;1m", "\x1b[31;1m", "\x1b[33;1m", "\x1b[32;1m",
-       "\x1b[36;1m", "\x1b[34;1m", "\x1b[35;1m", "\x1b[37;1m"};
+      {"\x1b[30;1m", "\x1b[31;1m", "\x1b[32;1m", "\x1b[33;1m",
+       "\x1b[34;1m", "\x1b[35;1m", "\x1b[36;1m", "\x1b[37;1m"};
   static const char* back[] =
-      {"\x1b[40;1m", "\x1b[41;1m", "\x1b[43;1m", "\x1b[42;1m",
-       "\x1b[46;1m", "\x1b[44;1m", "\x1b[45;1m", "\x1b[47;1m"};
+      {"\x1b[40;1m", "\x1b[41;1m", "\x1b[42;1m", "\x1b[43;1m",
+       "\x1b[44;1m", "\x1b[45;1m", "\x1b[46;1m", "\x1b[47;1m"};
+  static const char text[] = {'K', 'R', 'G', 'Y', 'B', 'M', 'C', 'W'};
   static const char* reset = "\x1b[0m";
-  static const char text[] = {'K', 'R', 'Y', 'G', 'C', 'B', 'M', 'W'};
-  const auto color = codel.color();
-  const auto bright = codel.brightness();
-  if (color == Color::UNKNOWN) {
-    std::cout << "?";
+  if (codel) {
+    std::cout << back[back_index(codel)]
+              << fore[fore_index(codel)]
+              << text[back_index(codel)]
+              << reset;
   } else {
-    const auto c = static_cast<int>(color);
-    const auto b = static_cast<int>(
-        bright == Brightness::LIGHT ? Color::WHITE :
-        bright == Brightness::DARK ? Color::BLACK : color);
-    std::cout << back[c] << fore[b] << text[c] << reset;
+    std::cout << "?";
   }
 }
 

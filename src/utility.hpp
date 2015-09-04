1fa019d0dd1a056507da06ee46f335ae9c5d88c5
hatsusato <knightspm5@gmail.com>
2015-09-04 20:07:21 +0900

    Add enumeration_traits

--- src/utility.hpp
+++ src/utility.hpp
@@ -1,6 +1,7 @@
 #ifndef PIET_I_UTILITY_HPP
 #define PIET_I_UTILITY_HPP
 
+#include <type_traits>
 #include <utility>
 
 namespace std {
@@ -14,6 +15,21 @@ template <typename T, typename... Args>
 std::unique_ptr<T, std::default_delete<T> > make_unique(Args&&... args) {
   return std::unique_ptr<T, std::default_delete<T> >(
       new T(std::forward<Args>(args)...));
+}
+
+template <typename E>
+struct enumeration_traits {};
+
+struct enumeration_trait_indexing {
+  static constexpr bool does_index = true;
+};
+
+template <typename E,
+          typename U = typename std::enable_if<
+            enumeration_traits<E>::does_index,
+            typename std::underlying_type<E>::type>::type>
+U operator+(E e) {
+  return static_cast<U>(e);
 }
 
 #endif  // PIET_I_UTILITY_HPP

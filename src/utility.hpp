1196e7ac09d1372ca8dfd4c54e5b11ab78f6cd5d
hatsusato <knightspm5@gmail.com>
2015-09-05 01:47:31 +0900

    Update methods related to enumeration_traits

--- src/utility.hpp
+++ src/utility.hpp
@@ -24,7 +24,7 @@ using enabler = typename std::enable_if<B, std::nullptr_t>::type;
   enabler<B> = nullptr
 
 template <typename E>
-struct enumeration_traits {};
+struct enumeration_traits;
 
 struct enumeration_trait_indexing {
   static constexpr bool does_index = true;
@@ -34,8 +34,35 @@ template <typename E,
           typename U = typename std::enable_if<
             enumeration_traits<E>::does_index,
             typename std::underlying_type<E>::type>::type>
-U operator+(E e) {
+constexpr U operator+(E e) {
   return static_cast<U>(e);
+}
+template <typename E,
+          ENABLER(enumeration_traits<E>::does_index)>
+constexpr E& operator++(E& e) {
+  return (e = static_cast<E>((+e + 1) % enumeration_traits<E>::count));
+}
+template <typename E, typename T,
+          ENABLER(enumeration_traits<E>::does_index)>
+constexpr E& operator+=(E& e, T value) {
+  using type = typename std::underlying_type<E>::type;
+  return (e = static_cast<E>(
+      (+e + enumeration_traits<E>::count + static_cast<type>(value))
+      % enumeration_traits<E>::count));
+}
+template <typename E,
+          typename U = typename std::enable_if<
+            enumeration_traits<E>::does_index,
+            typename std::underlying_type<E>::type>::type>
+constexpr U operator-(E lhs, E rhs) {
+  return ((enumeration_traits<E>::count + +lhs - +rhs)
+          % enumeration_traits<E>::count);
+}
+
+template <typename E,
+          ENABLER(enumeration_traits<E>::does_index)>
+constexpr auto index(E e) -> decltype(+e) {
+  return +e;
 }
 
 #endif  // PIET_I_UTILITY_HPP

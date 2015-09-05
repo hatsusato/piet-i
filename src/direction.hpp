9b74851bd7bf53c6b1d2635497815068194355d8
hatsusato <knightspm5@gmail.com>
2015-09-05 12:40:54 +0900

    Add Direction and Choose

--- src/direction.hpp
+++ src/direction.hpp
@@ -1,4 +1,16 @@
 #ifndef PIET_I_DIRECTION_HPP
 #define PIET_I_DIRECTION_HPP
 
+#include "utility.hpp"
+
+enum class Direction : int {
+  RIGHT, DOWN, LEFT, UP, COUNT
+};
+ENUM_INDEXING(Direction);
+
+enum class Choose : int {
+  LEFT, RIGHT, COUNT
+};
+ENUM_INDEXING(Choose);
+
 #endif  // PIET_I_DIRECTION_HPP

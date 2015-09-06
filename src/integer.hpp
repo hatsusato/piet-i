67096f780f814f668024052155860479fab6c889
hatsusato <knightspm5@gmail.com>
2015-09-07 00:21:54 +0900

    Add Integer

--- src/integer.hpp
+++ src/integer.hpp
@@ -1,4 +1,15 @@
 #ifndef PIET_I_INTEGER_HPP
 #define PIET_I_INTEGER_HPP
 
+#include <cstdint>
+
+struct Integer {
+ public:
+  Integer() : value_(0) {}
+  template <typename T>
+  explicit Integer(T value) : value_(value) {}
+ private:
+  intmax_t value_;
+};
+
 #endif  // PIET_I_INTEGER_HPP
